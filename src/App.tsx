5b1ac63ae29805f69433c809b78395c93aea673a
Leslie Leiter <leslieleiter@Mac-mini.local>
2026-03-18 11:07:57 -0500

    Replace MapPin with Facebook F SVG icon in contact section

--- src/App.tsx
+++ src/App.tsx
@@ -520,8 +520,10 @@ const Contact = () => {
                 </div>
               </div>
               <div className="flex items-start">
-                <div className="bg-zinc-950 p-3 mr-6">
-                  <MapPin size={24} />
+                <div className="bg-zinc-950 p-3 mr-6 flex items-center justify-center" style={{width: 48, height: 48}}>
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24 12.073C24 5.403 18.627 0 12 0S0 5.403 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.413c0-3.026 1.792-4.697 4.533-4.697 1.313 0 2.686.236 2.686.236v2.97h-1.513c-1.491 0-1.956.93-1.956 1.886v2.265h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
+                  </svg>
                 </div>
                 <div>
                   <span className="block text-xs uppercase tracking-widest font-bold opacity-70 mb-1">Find Us</span>
